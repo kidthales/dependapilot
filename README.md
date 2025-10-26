6ad85dedecd3943e8b1b276e736055a082f842b6
kidthales <kidthales@agogpixel.com>
2025-10-25 18:15:22 -0700

    Initial commit
    Create project with 'symfony new dependapilot --version="7.3.x"' command.
    Set php >=8.4 and adjust polyfills in composer.json file.
    Select the agpl-3.0 license for the project.
    Add a .gitattributes file.
    Add license header to available source code.
    Remove the env.dev file.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Dependapilot
+
+[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](./LICENSE)
+
+> [!NOTE]  
+> 🚧 Under Construction 🚧
---
+++ src/Kernel.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * (c) Tristan Bonsor <tristan@agogpixel.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App;
+
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+
+class Kernel extends BaseKernel
+{
+    use MicroKernelTrait;
+}

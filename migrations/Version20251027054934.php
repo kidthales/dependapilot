d084689f555486c6cf3ab751d4aaea45325252d1
kidthales <kidthales@agogpixel.com>
2025-10-26 23:17:48 -0700

    Adjust migration>

--- migrations/Version20251027054934.php
+++ migrations/Version20251027054934.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * (c) Tristan Bonsor <tristan@agogpixel.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 declare(strict_types=1);
 
 namespace DoctrineMigrations;
@@ -8,31 +15,41 @@ use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
 /**
- * Auto-generated Migration: Please modify to your needs!
+ * @author kidthales <kidthales@agogpixel.com>
  */
 final class Version20251027054934 extends AbstractMigration
 {
+    /**
+     * @return string
+     */
     public function getDescription(): string
     {
         return 'Create/Drop the user table.';
     }
 
+    /**
+     * @param Schema $schema
+     * @return void
+     */
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql(<<<'SQL'
             CREATE TABLE user (
               id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
               github_id BIGINT NOT NULL,
               created_at BIGINT NOT NULL
             )
-        SQL);
+        SQL
+        );
         $this->addSql('CREATE UNIQUE INDEX UNIQ_IDENTIFIER_GITHUB_ID ON user (github_id)');
     }
 
+    /**
+     * @param Schema $schema
+     * @return void
+     */
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP TABLE user');
     }
 }
